e10b2b9c121dd36ec6b1aa4e240a0d267ffe2a67
Gottumukkala Kanka Rju <gkr5413@gmail.com>
2020-04-09 22:48:11 +0530

    Update OS 8.c

--- os/OS 8.c
+++ os/OS 8.c
@@ -2,71 +2,66 @@
 #include<conio.h>
 int main()
  {
-   int x,n,p[10],pp[10],bt[10],w[10],t[10],awt,atat,i,at[10],tq;
-  
-   printf("Enter the number of process : ");
+   int A , n,  C[10], Q[10], B[10], D[10], V[10], Z , X, i, Y[10], T;
+    
+   printf(" number of process ? : ");
    scanf("%d",&n);
-   printf("Enter time quantum");
-   scanf("%d",&tq);
-   printf("\n\t Enter burst time : time priorities : Arrival time  \n");
+   printf("time quantum");
+   scanf("%d",&T);
+   printf("\n\t burst time : time priorities : Arrival time  \n");
  
    for(i=0;i<n;i++)
     {
       printf("\n Process %d ",i+1);
-      scanf("%d %d %d",&bt[i],&pp[i],&at[i]);
-	  p[i]=i+1;
+      scanf("%d %d %d",&B[i],&Q[i],&Y[i]);
+	  C[i]=i+1;
     }
- 
 int j;
   for(i=0;i<n-1;i++)
    {
      for(j=i+1;j<n;j++)
      {
-       if(pp[i]<pp[j])
+       if(Q[i]<Q[j])
        {
-     x=pp[i];
-     pp[i]=pp[j];
-     pp[j]=x;
-     x=bt[i];
-     bt[i]=bt[j];
-     bt[j]=x;
-     x=p[i];
-     p[i]=p[j];
-     p[j]=x;
+     A=Q[i];
+     Q[i]=Q[j];
+     Q[j]=A;
+     A=B[i];
+     B[i]=B[j];
+     B[j]=A;
+     A=C[i];
+     C[i]=C[j];
+     C[j]=A;
       }
    }
 }
-w[0]=0;
-awt=0;
-t[0]=bt[0];
-atat=t[0];
+D[0]=0;
+Z=0;
+V[0]=B[0];
+X=V[0];
 for(i=1;i<n;i++)
  {
- 	if(tq<n || tq>n) {
+ 	if( T<n || T>n ) {
 	 
-   w[i]=t[i-1];
-   awt+=w[i];
-   t[i]=w[i]+bt[i];
-   atat+=t[i];
+   D[i]=V[i-1];
+   Z+=D[i];
+   V[i]=D[i]+B[i];
+   X+=V[i];
 	 }
  }
- 
  printf("\n");
- printf("Gantt chart\n");
+ printf(" Then Gantt chart is \n");
  for(i=0;i<n;i++)
  {
- 	printf("P %d ",p[i]);
+ 	printf("P %d ",C[i]);
  }
-
- 
 printf("\n\nProcess \t Burst Time \t Wait Time \t Turn Around Time   Priority \tArrival time \n");
 for(i=0;i<n;i++){
-printf("\n  %d",p[i]); printf("\t\t %d",bt[i]); printf("\t\t %d",w[i]); printf("\t\t %d",t[i]); printf("\t\t %d",pp[i]); printf("\t\t %d",at[i]);
-
+printf("\n  %d",C[i]); printf("\t\t %d",B[i]); printf("\t\t %d",D[i]); printf("\t\t %d",V[i]); printf("\t\t %d",Q[i]); printf("\t\t %d",Y[i]);
 }
-awt/=n;
-atat/=n;
-printf("\n Average Wait Time : %d ",awt);
-printf("\n Average Turn Around Time : %d",atat);
+Z/=n;
+X/=n;
+printf("\n Average Waiting Time : %d ",Z);
+printf("\n Average TurnAround Time : %d",X);
 getch();
 }
